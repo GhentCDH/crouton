f8a81624245c17ed8dca736e059460cbed2d9085
Bo Vandersteene <bo@firepeak.tech>
2026-07-10 15:31:37 +0200

    fix: warning on displayvalue

--- packages/crouton-forms-vue/src/forms/renderers/controls/readonly/displayValue/displayValue.properties.ts
+++ packages/crouton-forms-vue/src/forms/renderers/controls/readonly/displayValue/displayValue.properties.ts
@@ -1,15 +1,7 @@
-import { type PropType } from 'vue';
-
 export const DisplayValueProperties = {
   direction: { type: String, default: 'ltr' },
   path: { type: String, required: true as const },
-  displayValue: {
-    type: [Object, String, Number, Boolean, Array, null] as PropType<unknown>,
-    required: true as const,
-  },
-  value: {
-    type: [Object, String, Number, Boolean, Array, null] as PropType<unknown>,
-    required: true as const,
-  },
+  displayValue: { required: true as const, default: null },
+  value: { required: true as const, default: null },
   options: { type: Object, required: true as const },
 };
