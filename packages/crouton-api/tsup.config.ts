b21d69247873621a7736362e2b40c977bdafc6e7
Bo Vandersteene <bo@firepeak.tech>
2026-07-15 12:14:53 +0200

    fix (crouton-api): fix cjs output

--- packages/crouton-api/tsup.config.ts
+++ packages/crouton-api/tsup.config.ts
@@ -8,6 +8,7 @@ export default defineConfig({
   splitting: false,
   sourcemap: true,
   clean: true,
+  shims: true,
   noExternal: ['@ghentcdh/crouton-core'],
   external: [
     '@nestjs/common',
