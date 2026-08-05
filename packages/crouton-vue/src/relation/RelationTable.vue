af50e25b01c8133c6de8f84f1ad742710e9b0cac
Bo Vandersteene <1673799+bovandersteene@users.noreply.github.com>
2026-08-05 15:46:21 +0200

    feat: add relation table renderer

--- packages/crouton-vue/src/relation/RelationTable.vue
+++ packages/crouton-vue/src/relation/RelationTable.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { TableComponent } from '@ghentcdh/crouton-forms-vue';
 import { RelationTableProperties } from './RelationTable.properties';
+import { Btn, IconEnum } from '@ghentcdh/ui';
 
 const props = defineProps(RelationTableProperties);
 
@@ -8,14 +9,20 @@ props.resource.reload();
 </script>
 <template>
   <fieldset class="fieldset">
-    <legend
-      class="w-full inline-block"
-      :class="['fieldset-legend', 'p-0 h-0 leading-none']"
-    >
+    <legend class="w-full inline-block" :class="['fieldset-legend']">
       {{ label }}
     </legend>
     <div>
       <TableComponent v-bind="resource" :data="values" />
+      <div class="mt-2">
+        <Btn
+          v-if="resource.operations.create"
+          :icon="IconEnum.Plus"
+          @click="resource.create"
+        >
+          <span class="whitespace-nowrap">Add record</span>
+        </Btn>
+      </div>
     </div>
   </fieldset>
 </template>
